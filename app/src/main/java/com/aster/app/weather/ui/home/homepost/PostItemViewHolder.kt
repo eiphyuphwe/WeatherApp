b82d2c0fbe732dc869ac9ad4a48405610dd2269f
Ei Phyu <eiphyuphwe643@gmail.com>
2021-03-06 11:13:45 +0800

    Get data from db instead of directly from network

--- app/src/main/java/com/aster/app/weather/ui/home/homepost/PostItemViewHolder.kt
+++ app/src/main/java/com/aster/app/weather/ui/home/homepost/PostItemViewHolder.kt
@@ -9,10 +9,10 @@ import com.aster.app.weather.R
 import com.aster.app.weather.data.model.Post
 import com.aster.app.weather.di.component.ViewHolderComponent
 import com.aster.app.weather.ui.base.BaseItemViewHolder
-import com.aster.app.weather.utils.common.GlideHelper
-import kotlinx.android.synthetic.main.item_view_post.view.*
 
-class PostItemViewHolder(parent: ViewGroup): BaseItemViewHolder<Post, PostItemViewModel>(R.layout.item_view_post,parent)
+
+
+class PostItemViewHolder(parent: ViewGroup): BaseItemViewHolder<Post, PostItemViewModel>(R.layout.item_view_weather_time_snapshots,parent)
 {
     override fun injectDependencies(viewHolderComponent: ViewHolderComponent) {
         viewHolderComponent.inject(this)
